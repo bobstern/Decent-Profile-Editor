a309858f0e2b49136dfc2f54f0280534d2742bac
bobstern <bobstern@users.noreply.github.com>
2021-11-08 16:51:36 -0800

    Step View Builder: Index out of range bug fixed in button views by enclosing entire steps view in condition: if idx < vm.newProfile.shotSteps.count {

--- Decent Profile Editor/Views/Step View Builder.swift
+++ Decent Profile Editor/Views/Step View Builder.swift
@@ -11,22 +11,22 @@ struct StepViewBuilder: View {
     @ObservedObject var vm : ViewModel
     @Binding var deleteStepAlert : Bool
     @Binding var actionIdx : Int?  // used by Delete alert dialog.
-// @State private var stepBindCondx : Binding<ShotStep>
-//    let forEachIndex : Int
-//    var idx : Int {
-//        if forEachIndex < vm.newProfile.shotSteps.count {
-//            return forEachIndex
-//        } else {
-//            return 0
-//        }
-//    }
-//    var stepBindingSafe : Binding<ShotStep> {
-//        if forEachIndex < vm.newProfile.shotSteps.count {
-//            return $vm.newProfile.shotSteps[forEachIndex]
-//        } else {
-//            return $vm.dummyShotStep  //$vm.newProfile.shotSteps[0]
-//        }
-//    }
+    // @State private var stepBindCondx : Binding<ShotStep>
+    //    let forEachIndex : Int
+    //    var idx : Int {
+    //        if forEachIndex < vm.newProfile.shotSteps.count {
+    //            return forEachIndex
+    //        } else {
+    //            return 0
+    //        }
+    //    }
+    //    var stepBindingSafe : Binding<ShotStep> {
+    //        if forEachIndex < vm.newProfile.shotSteps.count {
+    //            return $vm.newProfile.shotSteps[forEachIndex]
+    //        } else {
+    //            return $vm.dummyShotStep  //$vm.newProfile.shotSteps[0]
+    //        }
+    //    }
     let idx: Int
     func stepBindingSafe(idx: Int) -> Binding<ShotStep> {
         if idx < vm.newProfile.shotSteps.count {
@@ -38,8 +38,9 @@ struct StepViewBuilder: View {
     
     @ViewBuilder
     var body: some View {
-            let stepBinding = stepBindingSafe(idx: idx) // $vm.newProfile.shotSteps[idx]
+        let stepBinding = stepBindingSafe(idx: idx) // $vm.newProfile.shotSteps[idx]
         
+        if idx < vm.newProfile.shotSteps.count {
             // HStack has all UX elements except Swap button:
             HStack {
                 Group {
@@ -82,15 +83,20 @@ struct StepViewBuilder: View {
                 }.frame(width: 130, alignment: .trailing)
                 
                 // ZStack used merely to apply frame to align right boundary of blank space before buttons.
+                // idx out of range bug fixed by first "if":
                 ZStack {
+                    //                    if idx < vm.newProfile.shotSteps.count, vm.newProfile.shotSteps[idx].exitType == .zero {
                     if vm.newProfile.shotSteps[idx].exitType == .zero {
                         EmptyView()
                     } else {
                         TextField("", text: stepBinding.exitVal).frame(width: 44, alignment: .center).padding(.trailing, 8)
                     }
                 }.frame(width: 100, alignment: .leading) // aligns buttons at right
+                // end ZStack
                 
+                // HStack continued:
                 // Buttons have distinct style:
+                // if idx < vm.newProfile.shotSteps.count {
                 Group {
                     /// "Duplicate" button:
                     Button(action: {duplicateStep(atIndex: idx)},
@@ -104,22 +110,24 @@ struct StepViewBuilder: View {
                         label: {Image("trash-regular").resizable().frame(width: 23, height: 23)}
                     )
                 }.buttonStyle(PlainButtonStyle())
+                //                }
                 Spacer()
-            }.multilineTextAlignment(.center).frame(height: 40) // end HStack
+            }.multilineTextAlignment(.center).frame(height: 40)
+            // end HStack
             
             // "Swap" button = row alternating w/ preceding HStack:
-            if idx < vm.newProfile.shotSteps.count-1 {
+            if idx < vm.newProfile.shotSteps.count-1 {  // Omit after last row.
                 HStack{
                     Button(action: { moveDownOneStep(fromIndex: idx) },
                            label: {Image("arrow.triangle.swap-heavy").resizable().frame(width: 22, height: 22).opacity(0.5) } )
                         .padding(.leading, 10)
                         .frame(height: 40)
                         .buttonStyle(PlainButtonStyle()) // no border
-
+                    
                     Spacer()
                 }
-            }
-        // } // end if idx < count
-        }
-    }
-
+            }// end Swap Button HStack
+            
+        } // end "if idx < count-1"
+    }  // end @ViewBuilder body
+}
